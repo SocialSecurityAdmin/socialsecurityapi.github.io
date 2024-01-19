93d6774930c1c44a7f53aaaee91602ee5a1ad239
SocialSecurityAdmin <susanlharper01@gmail.com>
2024-01-19 11:13:51 -0800

    connect

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;  //this is our port number
 
 require('dotenv').config()
 
-const CONN_URL = process.env.CONN_URL
+const uri = process.env.CONN_URL
 const app = express(); // this is our app or instance of express
 app.use(cors())
 
@@ -47,7 +47,7 @@ app.post('/formPost', async (req,res) => {
 
 
 //This is basically to listen on port 5000
-mongoose.connect(CONN_URL).then(() => {
+mongoose.connect(uri).then(() => {
     app.listen(port, ()=>{
         console.log(`server started at http://localhost:${port}`)
     });
